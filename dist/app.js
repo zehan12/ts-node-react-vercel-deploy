7acbcbc0c06826b3c168b2257b49c08a93c3505c
Zehan Khan <zkhan@hootboard.com>
2023-11-04 02:26:22 +0530

    add react to server

--- dist/app.js
+++ dist/app.js
@@ -2,12 +2,16 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const tslib_1 = require("tslib");
 const express_1 = tslib_1.__importDefault(require("express"));
+const compression_1 = tslib_1.__importDefault(require("compression"));
+const path_1 = tslib_1.__importDefault(require("path"));
 const app = (0, express_1.default)();
+app.use((0, compression_1.default)());
+app.use(express_1.default.static(path_1.default.join(__dirname, "/../build")));
 app.get("/api", (req, res) => {
     res.status(200).json({ info: "api is working threw serverless function" });
 });
 app.get("/", (req, res) => {
-    res.end("TS NODE with CI CD");
+    res.sendFile("../build/index.html");
 });
 exports.default = app;
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,18 @@
 import express, { Express, Request, Response } from "express";
+import compression from "compression";
+import path from "path";
 
 const app: Express = express();
+
+app.use(compression());
+app.use(express.static(path.join(__dirname, "/../build")));
 
 app.get("/api", (req: Request, res: Response) => {
   res.status(200).json({ info: "api is working threw serverless function" });
 });
 
 app.get("/", (req: Request, res: Response) => {
-  res.end("TS NODE with CI CD");
+    res.sendFile("../build/index.html");
 });
 
 export default app;
