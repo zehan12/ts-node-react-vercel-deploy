a4df9cbc40d39efb8940fcf3842b260d21358002
Zehan Khan <zkhan@hootboard.com>
2023-11-04 15:04:30 +0530

    change dist

--- dist/app.js
+++ dist/app.js
@@ -3,22 +3,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const tslib_1 = require("tslib");
 const express_1 = tslib_1.__importDefault(require("express"));
 const compression_1 = tslib_1.__importDefault(require("compression"));
-const path_1 = tslib_1.__importDefault(require("path"));
 const helmet_1 = tslib_1.__importDefault(require("helmet"));
 const app = (0, express_1.default)();
 app.use(helmet_1.default.contentSecurityPolicy({
     directives: {
-        defaultSrc: ["'self'"],
-        styleSrc: ["'self'", "https://www.gstatic.com"] } }));
+        defaultSrc: ["'self'"] } }));
 
 
 app.use((0, compression_1.default)());
-app.use(express_1.default.static(path_1.default.join(__dirname, "/../build")));
 app.get("/api", (req, res) => {
     res.status(200).json({ info: "api is working threw serverless function" });
 });
 app.get("/", (req, res) => {
-    res.sendFile("../build/index.html");
+    res.status(200).json({ success: "backend is working" });
 });
 exports.default = app;
 //# sourceMappingURL=app.js.map
